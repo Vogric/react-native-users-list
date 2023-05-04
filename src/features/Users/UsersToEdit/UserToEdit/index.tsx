eeaa23bdd7a285709c4f593149c352bcb3a89f61
Vogric <vogric@eze-lln-B02977.synapse.com>
2023-05-04 00:16:57 -0300

    Add UserToEdit interface

--- src/features/Users/UsersToEdit/UserToEdit/index.tsx
+++ src/features/Users/UsersToEdit/UserToEdit/index.tsx
@@ -1,9 +1,16 @@
+import { FC } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import styles from './styles';
 import { theme } from '@src/theme';
+import { UserDetail } from '@src/types';
 
-const UserToEdit = ({ item, handleOnPress }: any) => {
+interface UserToEditProps {
+  item: UserDetail,
+  handleOnPress: () => void
+}
+
+const UserToEdit: FC<UserToEditProps> = ({ item, handleOnPress }):JSX.Element => {
   return (
     <View style={styles.userToEditContainer}>
       <Text style={styles.userToEdit}>{item.login}</Text>
